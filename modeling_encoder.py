64c69eb083708ca333f731754d4424c323f28f3f
nawnoes <nawnoes@gmail.com>
2021-06-16 18:17:29 +0900

    poly encoder

--- modeling_encoder.py
+++ modeling_encoder.py
@@ -36,6 +36,25 @@ class PolyEncoder(nn.Module):
         if labels is None:
             response_input_ids = response_input_ids[:,0,:].unsqueeze(1)
             response_input_masks = response_input_masks[:, 0, :].unsqueeze(1)
-        
+
+        context_vec = self.lm_model(context_input_ids, context_input_masks)[0][:,0,:] # [batch_size, hidden_size]
+
         batch_size, res_cnt,seq_length = response_input_ids.shape # res_cnt is 1 during training
+        response_input_ids = response_input_ids.view(-1, seq_length)
+        response_input_masks = response_input_masks.view(-1, seq_length)
+
+        response_vec = self.lm_model(response_input_ids, response_input_masks)[0][:,0,:] # [batch_size, hidden_size]
+        response_vec = response_vec.view(batch_size, res_cnt, -1)
+
+        if labels is not None:
+            response_vec = response_vec.squeeze(1)
+            dot_product = torch.matmul(context_vec)
+            mask = torch.eye(context_input_ids.size(0)).to(context_input_ids.device)
+            loss = F.log_softmax(dot_product, dim=-1) * mask
+            loss = (-loss.sum(dim=1)).mean()
+
+            return loss
+        else:
+            context_vec = context_vec.unsqueez(1)
+
         
